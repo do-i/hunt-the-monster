86156ad8eebe87034fbce79a9b4575b1aa1ad5a0
Acorn Ninja <acorn@ninja.net>
2012-06-10 10:34:43 -0500

    Added Javadoc to DevDebugTool

--- src/main/java/org/djd/fun/huntthemonster/DevDebugTool.java
+++ src/main/java/org/djd/fun/huntthemonster/DevDebugTool.java
@@ -1,27 +1,38 @@
 package org.djd.fun.huntthemonster;
 
+/**
+ * 
+ * This class is a debugging tool. It displays dungeon and location of monster
+ * and the player.
+ * 
+ * <p>
+ * 
+ * <li>0 indicates no room
+ * <li>1 indicates a room
+ * <li>m indicates a monster
+ * <li>p indicates a player
+ * 
+ */
 public class DevDebugTool {
 
-	Dungeon dungeon;
-	Chara charaMonster;
-	Player player;
+   Dungeon dungeon;
+   Chara charaMonster;
+   Player player;
 
-	public DevDebugTool(Dungeon dungeon, Chara charaMonster, Player player) {
-		this.dungeon = dungeon;
-		this.charaMonster = charaMonster;
-		this.player = player;
-	}
+   public DevDebugTool(Dungeon dungeon, Chara charaMonster, Player player) {
+      this.dungeon = dungeon;
+      this.charaMonster = charaMonster;
+      this.player = player;
+   }
 
-	public void debugInfo() {
-		System.out.println("============================================");
-		System.out.println("Dungeon");
-		dungeon.debugPrintDungeonMapWithCharas(charaMonster, player);
+   public void debugInfo() {
+      System.out.println("============================================");
+      System.out.println("Dungeon");
+      dungeon.debugPrintDungeonMapWithCharas(charaMonster, player);
 
-		System.out.println(String.format("player location (%d,%d)", player.x,
-				player.y));
-		System.out.println(String.format("monster location (%d,%d)",
-				charaMonster.x, charaMonster.y));
-		System.out.println("============================================");
-		
-	}
+      System.out.println(String.format("player location (%d,%d)", player.x, player.y));
+      System.out.println(String.format("monster location (%d,%d)", charaMonster.x, charaMonster.y));
+      System.out.println("============================================");
+
+   }
 }
