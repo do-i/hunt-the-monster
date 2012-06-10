294bb412fcce925fe2110a0b7fd7d7a8064eabb5
Acorn Ninja <acorn@ninja.net>
2012-06-10 11:27:30 -0500

    Added javadoc to Direction

--- src/main/java/org/djd/fun/huntthemonster/Direction.java
+++ src/main/java/org/djd/fun/huntthemonster/Direction.java
@@ -1,5 +1,20 @@
 package org.djd.fun.huntthemonster;
 
+/**
+ * Direction offset based on 2 dimentional array <code>area = new [y][x]</code>.
+ * 
+ * <pre>
+ * [0,0][1,0][2,0]...[n,0] 
+ * [0,1][1,1][2,1]...[n,1] 
+ * [0,2][1,2][2,2]...[n,2] 
+ *       .
+ *       .
+ * [0,n][1,n][2,n]...[n,n] 
+ * </pre>
+ * 
+ * @author acorn
+ *
+ */
 public enum Direction {
 
 	N(0, -1), E(1, 0), S(0, 1), W(-1, 0);
